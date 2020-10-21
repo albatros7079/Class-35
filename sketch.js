3f3107554f5e4b8c0e9dc4dd559e37e160c04803
albatross7079 <70518748+albatros7079@users.noreply.github.com>
2020-10-21 17:18:30 +0530

    Add files via upload

---
+++ sketch.js
@@ -0,0 +1,14 @@
+var gameState, playerCount, database;
+var form, player, game;
+
+
+function setup(){
+  database = firebase.database();
+  console.log(database);
+  createCanvas(500,500);
+
+}
+
+function draw(){
+  
+}
